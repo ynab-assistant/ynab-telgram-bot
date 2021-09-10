51d6cb4c5e8b1b0c65ff10a24e0e284d14adc3f3
alexeybo <lex.nox@gmail.com>
2021-09-11 01:27:08 +0300

    Updated tests for Prior parser

--- bot/pkg/smsparser/prior/prior.go
+++ bot/pkg/smsparser/prior/prior.go
@@ -11,7 +11,16 @@ import (
 )
 
 const (
-	priorPrefix = "Priorbank"
+	priorPrefix         = "Priorbank"
+	cardNumberIndx      = 1
+	transactionTypeIndx = 0
+	transactionDateIndx = 2
+	transactionTimeIndx = 3
+	currencyIndx        = 2
+	countryCodeIndx     = 0
+	payeeIndx1          = 1
+	payeeIndx2          = 2
+	amountIndx          = 1
 )
 
 // Prior is an implementation of SMS Parser for Priorbank
@@ -24,6 +33,8 @@ func NewPrior(log *log.Logger) *Prior {
 	return &Prior{log: log}
 }
 
+// Parse parses the specified text and returns SmsMessage if there are no errors.
+// if a field could not be parsed, it will be set by zero value (empty, 0, etc)
 func (p *Prior) Parse(text string) (smsparser.SmsMessage, error) {
 	if !strings.HasPrefix(text, priorPrefix) {
 		return smsparser.SmsMessage{}, errors.New("SMS either not from Priorbank or format changed")
@@ -31,63 +42,57 @@ func (p *Prior) Parse(text string) (smsparser.SmsMessage, error) {
 
 	msg := smsparser.SmsMessage{}
 	chunks := strings.Split(text, ". ")
+
+	// a message from Priorbank should be splited into the following 5 chunks:
+	// Priorbank
+	// Karta 4***3345 10-09-2021 15:40:19
+	// Oplata 38.96 BYN
+	// BLR SHOP SOSEDI
+	// Spravka: 80171199900
 	if len(chunks) < 5 {
 		return smsparser.SmsMessage{}, errors.New("SMS either not from Priorbank or format changed")
 	}
 
 	msg.BankName = chunks[0]
 
-	msg.CardNumber = p.getCardNumber(chunks[1])
-	msg.Transaction.Date = p.getTransactionDate(chunks[1])
+	msg.CardNumber = p.parseValue(chunks[1], cardNumberIndx)
+	msg.Transaction.Date = p.parseCompositeValue(chunks[1], transactionDateIndx, transactionTimeIndx)
 
-	msg.Transaction.Type = p.getTransactionType(chunks[2])
-	msg.Amount = p.getAmount(chunks[2])
-	msg.Currency = p.getCurrency(chunks[2])
+	// TODO: add validation for chanks lenght. Otherwise it may happen when card number will contain transaction date if kard number is missed.
+	msg.Transaction.Type = p.parseValue(chunks[2], transactionTypeIndx)
+	msg.Amount = p.getAmount(chunks[2], amountIndx)
+	msg.Currency = p.parseValue(chunks[2], currencyIndx)
 
-	msg.CountryCode = p.getCountryCode(chunks[3])
-	msg.Payee = p.getPayee(chunks[3])
+	msg.CountryCode = p.parseValue(chunks[3], countryCodeIndx)
+	msg.Payee = p.parseCompositeValue(chunks[3], payeeIndx1, payeeIndx2)
 
 	msg.OriginalMsg = text
 
 	return msg, nil
 }
 
-func (p *Prior) getCardNumber(text string) string {
-	cardChunks := strings.Split(text, " ")
-	return cardChunks[1]
+func (p *Prior) parseValue(text string, valueIndex int) string {
+	chunks := strings.Split(text, " ")
+	if valueIndex > len(chunks) {
+		return ""
+	}
+	return chunks[valueIndex]
 }
 
-func (p *Prior) getTransactionDate(text string) string {
-	cardChunks := strings.Split(text, " ")
-	return fmt.Sprintf("%s %s", cardChunks[2], cardChunks[3])
+func (p *Prior) parseCompositeValue(text string, indx1, indx2 int) string {
+	chunks := strings.Split(text, " ")
+	if indx1 > len(chunks)-1 || indx2 > len(chunks)-1 {
+		return ""
+	}
+	return fmt.Sprintf("%s %s", chunks[indx1], chunks[indx2])
 }
 
-func (p *Prior) getTransactionType(text string) string {
-	transAndAmountChunks := strings.Split(text, " ")
-	return transAndAmountChunks[0]
-}
-
-func (p *Prior) getAmount(text string) float64 {
-	transAndAmountChunks := strings.Split(text, " ")
-	amount, err := strconv.ParseFloat(transAndAmountChunks[1], 64)
+func (p *Prior) getAmount(text string, amountIndx int) float64 {
+	amountStr := p.parseValue(text, amountIndx)
+	amount, err := strconv.ParseFloat(amountStr, 64)
 	if err != nil {
 		p.log.Printf("cant parse a line for transaction amount\n\t\tInvalid text:%s. err: %v", text, err)
 		return 0
 	}
 	return amount
-}
-
-func (p *Prior) getCurrency(text string) string {
-	transAndAmountChunks := strings.Split(text, " ")
-	return transAndAmountChunks[2]
-}
-
-func (p *Prior) getCountryCode(text string) string {
-	transAndAmountChunks := strings.Split(text, " ")
-	return transAndAmountChunks[0]
-}
-
-func (p *Prior) getPayee(text string) string {
-	chunks := strings.Split(text, " ")
-	return fmt.Sprintf("%s %s", chunks[1], chunks[2])
 }
--- bot/pkg/smsparser/prior/prior_test.go
+++ bot/pkg/smsparser/prior/prior_test.go
@@ -7,47 +7,108 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestParse(t *testing.T) {
-	t.Run("should return error when message does not start from Prior", func(t *testing.T) {
-		prior := Prior{}
+func TestParse_verifyErrors(t *testing.T) {
+	testTable := []struct {
+		name    string
+		smsText string
+		errText string
+	}{
+		{
+			name:    "should return error when message does not start from Prior",
+			smsText: "some text",
+			errText: "SMS either not from Priorbank or format changed",
+		},
+		{
+			name:    "should return error when message is empty",
+			smsText: "",
+			errText: "SMS either not from Priorbank or format changed",
+		},
+		{
+			name:    "should return error when message was split by a wrong amount of chunks",
+			smsText: "Priorbank. Karta 4***3345 10-09-2021 15:40:19. Oplata 38.96 BYN. BLR SHOP SOSEDI",
+			errText: "SMS either not from Priorbank or format changed",
+		},
+	}
 
-		result, err := prior.Parse("some text")
+	for _, testCase := range testTable {
+		t.Run(testCase.name, func(t *testing.T) {
+			prior := Prior{}
 
-		assert.Empty(t, result)
-		assert.EqualError(t, err, "SMS either not from Priorbank or format changed")
-	})
+			result, err := prior.Parse(testCase.smsText)
+			assert.Empty(t, result)
+			assert.EqualError(t, err, testCase.errText)
+		})
+	}
+}
+func TestParse2(t *testing.T) {
 
-	t.Run("should return error when message is empty", func(t *testing.T) {
-		prior := Prior{}
-
-		result, err := prior.Parse("")
-
-		assert.Empty(t, result)
-		assert.EqualError(t, err, "SMS either not from Priorbank or format changed")
-	})
-
-	t.Run("should parse message to struct", func(t *testing.T) {
-		msg := `Priorbank. Karta 4***3345 10-09-2021 15:40:19. Oplata 38.96 BYN. BLR SHOP SOSEDI.   Spravka: 80171199900`
-
-		expectedResult := smsparser.SmsMessage{
-			BankName:    "Priorbank",
-			CardNumber:  "4***3345",
-			Currency:    "BYN",
-			Amount:      38.96,
-			CountryCode: "BLR",
-			Payee:       "SHOP SOSEDI",
-			OriginalMsg: msg,
-			Transaction: smsparser.Transaction{
-				Type: "Oplata",
-				Date: "10-09-2021 15:40:19",
+	testTable := []struct {
+		name           string
+		smsText        string
+		expectedSmsMsg smsparser.SmsMessage
+	}{
+		{
+			name:    "should parse message to struct",
+			smsText: `Priorbank. Karta 4***3345 10-09-2021 15:40:19. Oplata 38.96 BYN. BLR SHOP SOSEDI.   Spravka: 80171199900`,
+			expectedSmsMsg: smsparser.SmsMessage{
+				BankName:    "Priorbank",
+				CardNumber:  "4***3345",
+				Currency:    "BYN",
+				Amount:      38.96,
+				CountryCode: "BLR",
+				Payee:       "SHOP SOSEDI",
+				OriginalMsg: "Priorbank. Karta 4***3345 10-09-2021 15:40:19. Oplata 38.96 BYN. BLR SHOP SOSEDI.   Spravka: 80171199900",
+				Transaction: smsparser.Transaction{
+					Type: "Oplata",
+					Date: "10-09-2021 15:40:19",
+				},
 			},
-		}
+		},
+		{
+			name:    "should parse message when transaction date is missed",
+			smsText: `Priorbank. Karta 4***3345. Oplata 38.96 BYN. BLR SHOP SOSEDI.   Spravka: 80171199900`,
+			expectedSmsMsg: smsparser.SmsMessage{
+				BankName:    "Priorbank",
+				CardNumber:  "4***3345",
+				Currency:    "BYN",
+				Amount:      38.96,
+				CountryCode: "BLR",
+				Payee:       "SHOP SOSEDI",
+				OriginalMsg: "Priorbank. Karta 4***3345. Oplata 38.96 BYN. BLR SHOP SOSEDI.   Spravka: 80171199900",
+				Transaction: smsparser.Transaction{
+					Type: "Oplata",
+					Date: "",
+				},
+			},
+		},
+		{
+			name:    "should parse message to struct when card number is missed",
+			smsText: `Priorbank. Karta 10-09-2021 15:40:19. Oplata 38.96 BYN. BLR SHOP SOSEDI.   Spravka: 80171199900`,
+			expectedSmsMsg: smsparser.SmsMessage{
+				BankName:    "Priorbank",
+				CardNumber:  "",
+				Currency:    "BYN",
+				Amount:      38.96,
+				CountryCode: "BLR",
+				Payee:       "SHOP SOSEDI",
+				OriginalMsg: "Priorbank. Karta 10-09-2021 15:40:19. Oplata 38.96 BYN. BLR SHOP SOSEDI.   Spravka: 80171199900",
+				Transaction: smsparser.Transaction{
+					Type: "Oplata",
+					Date: "10-09-2021 15:40:19",
+				},
+			},
+		},
+	}
 
-		prior := Prior{}
+	for _, testCase := range testTable {
+		t.Run(testCase.name, func(t *testing.T) {
+			prior := Prior{}
 
-		result, err := prior.Parse(msg)
+			result, err := prior.Parse(testCase.smsText)
 
-		assert.Nil(t, err)
-		assert.Equal(t, expectedResult, result)
-	})
+			assert.Nil(t, err)
+			assert.Equal(t, testCase.expectedSmsMsg, result)
+		})
+	}
+
 }
